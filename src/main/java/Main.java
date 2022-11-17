5bba5e983bd6aadde3e3d55b2208636ab831c3bb
Lev <fedorichev16@gmail.com>
2022-11-17 20:53:56 +0300

    pull request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,26 @@
 
-import java.util.List;
+import java.io.BufferedOutputStream;
+
+import java.util.logging.Handler;
 
 public class Main {
     public static void main(String[] args) {
-        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
-        Server server = new Server(validPaths, 9999);
-        server.start();
+        final var server = new Server();
+        // код инициализации сервера (из вашего предыдущего ДЗ)
+
+        // добавление handler'ов (обработчиков)
+        // Я так понимаю ошибки задуманы, так как в задании указано, что это функтор
+        server.addHandler("GET", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
+
+        server.listen(9999);
     }
 }
