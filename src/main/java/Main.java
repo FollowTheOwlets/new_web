e9d4d6434cab8238e720c87f81866d1c2d1d8bf0
Lev <fedorichev16@gmail.com>
2022-11-20 23:52:55 +0300

    second update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,32 @@
 
-import java.io.BufferedOutputStream;
-
-import java.util.logging.Handler;
-
 public class Main {
     public static void main(String[] args) {
         final var server = new Server();
-        // код инициализации сервера (из вашего предыдущего ДЗ)
+        var port = 9999;
+        Request.host = "http://localhost:" + port;
 
         // добавление handler'ов (обработчиков)
-        // Я так понимаю ошибки задуманы, так как в задании указано, что это функтор
-        server.addHandler("GET", "/messages", new Handler() {
-            public void handle(Request request, BufferedOutputStream responseStream) {
-                // TODO: handlers code
-            }
-        });
-        server.addHandler("POST", "/messages", new Handler() {
-            public void handle(Request request, BufferedOutputStream responseStream) {
-                // TODO: handlers code
-            }
-        });
+        // Handler просил переопределение 4-х методов поэтому свой интерфейс сделал
+        server
+                .addHandler("GET", "/messages", (request, responseStream) -> {
+                    responseStream.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Length: 0\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    responseStream.flush();
+                })
+                .addHandler("POST", "/messages", (request, responseStream) -> {
+                    responseStream.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Length: 0\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    responseStream.flush();
+                });
 
-        server.listen(9999);
+        server.listen(port);
     }
 }
--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,13 +1,83 @@
-import java.util.Map;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Request {
+    public static String host;
     final String method;
-    final Map<String,String> headers;
+    final String path;
+    final List<String> headers;
     final String body;
 
-    public Request(String method, Map<String,String> headers, String body) {
+    public Request(String method, String path, List<String> headers) {
         this.method = method;
+        this.path = path;
+        this.headers = headers;
+        this.body = "";
+        System.out.println(getQueryParams());
+    }
+
+    public Request(String method, String path, List<String> headers, String body) {
+        this.method = method;
+        this.path = path;
         this.headers = headers;
         this.body = body;
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        try {
+            return URLEncodedUtils.parse(new URI(host + path), Charset.defaultCharset());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(host + path + " ошибка декодирования");
+        }
+    }
+
+    public List<String> getQueryParam(String name) {
+        List<String> list = new ArrayList<>();
+        for (NameValuePair pair : getQueryParams()) {
+            if (pair.getName().equals(name)) {
+                list.add(pair.getValue());
+            }
+        }
+        return list;
+    }
+
+    public List<NameValuePair> getPostParams() {
+        String encodeBody = URLDecoder.decode(body, StandardCharsets.UTF_8);
+
+        String[] pairs = encodeBody.split("&");
+        NameValuePair[] nameValuePairs = new NameValuePair[pairs.length];
+        for (int i = 0; i < pairs.length; i++) {
+            String pair = pairs[i];
+            nameValuePairs[i] = new NameValuePair() {
+                @Override
+                public String getName() {
+                    return pair.substring(0, pair.indexOf("="));
+                }
+
+                @Override
+                public String getValue() {
+                    return pair.substring(pair.indexOf("=") + 1);
+                }
+            };
+        }
+        return List.of(nameValuePairs);
+    }
+
+    public List<String> getPostParam(String name) {
+        List<String> list = new ArrayList<>();
+        for (NameValuePair pair : getPostParams()) {
+            if (pair.getName().equals(name)) {
+                list.add(pair.getValue());
+            }
+        }
+        return list;
     }
 }
